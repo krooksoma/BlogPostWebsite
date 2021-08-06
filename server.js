3bca284338d600ac5ae2750fc978bd60f3c594fe
Romulo <romulojusto@gmail.com>
2021-08-06 13:12:58 -0400

    Updated routes and views

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // creates connection to the server
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const hbs = exphbs.create({ helpers });
 
